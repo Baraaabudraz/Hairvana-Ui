52a6d6537df2d9879b87151ee8b90961ffba90a0
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-07-01 12:06:04 +0300

    Create 20250630083928-create-staff.js

---
+++ backend/migrations/20250630083928-create-staff.js
@@ -0,0 +1,52 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Staffs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      phone: {
+        type: Sequelize.STRING
+      },
+      salon_id: {
+        type: Sequelize.INTEGER
+      },
+      role: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      avatar: {
+        type: Sequelize.STRING
+      },
+      services: {
+        type: Sequelize.JSON
+      },
+      schedule: {
+        type: Sequelize.JSON
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Staffs');
+  }
+};

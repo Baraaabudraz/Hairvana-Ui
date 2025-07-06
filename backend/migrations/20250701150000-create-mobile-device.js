1612b760e454e2a516c85962e87541dd7c25cb73
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-07-06 16:16:19 +0300

    Update 20250701150000-create-mobile-device.js

--- backend/migrations/20250701150000-create-mobile-device.js
+++ backend/migrations/20250701150000-create-mobile-device.js
@@ -5,7 +5,7 @@ module.exports = {
     await queryInterface.createTable('mobile_devices', {
       id: {
       type: Sequelize.UUID,
-      defaultValue: Sequelize.literal('gen_random_uuid()'),
+      defaultValue: Sequelize.literal('uuid_generate_v4()'),
       primaryKey: true
     },
     user_id: {

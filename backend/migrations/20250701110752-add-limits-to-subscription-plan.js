40813dc54138d70f2ac71f457565e572a06c9bfb
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-07-07 12:32:32 +0300

    Update 20250701110752-add-limits-to-subscription-plan.js

--- backend/migrations/20250701110752-add-limits-to-subscription-plan.js
+++ backend/migrations/20250701110752-add-limits-to-subscription-plan.js
@@ -3,11 +3,18 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn('subscription_plans', 'limits', {
-      type: Sequelize.JSON,
-      allowNull: true,
-      defaultValue: null
-    });
+    await queryInterface.sequelize.query(`
+      DO $$
+      BEGIN
+        IF NOT EXISTS (
+          SELECT 1 FROM information_schema.columns
+          WHERE table_name='subscription_plans' AND column_name='limits'
+        ) THEN
+          ALTER TABLE "subscription_plans" ADD COLUMN "limits" JSON;
+        END IF;
+      END
+      $$;
+    `);
   },
 
   async down (queryInterface, Sequelize) {

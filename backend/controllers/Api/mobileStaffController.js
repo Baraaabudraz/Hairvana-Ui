377c954f1962994b68db019d78738b006846bac4
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-07-08 16:08:15 +0300

    Create mobileStaffController.js

---
+++ backend/controllers/Api/mobileStaffController.js
@@ -0,0 +1,25 @@
+const { Staff } = require('../../models');
+
+// List staff for a salon by salon_id in the path
+exports.getStaffForSalon = async (req, res, next) => {
+  try {
+    const { salon_id } = req.params;
+    if (!salon_id) return res.status(400).json({ message: 'salon_id is required' });
+    const staff = await Staff.findAll({ where: { salon_id } });
+    res.json(staff);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Get staff details by staff ID
+exports.getStaffById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const staff = await Staff.findByPk(id);
+    if (!staff) return res.status(404).json({ message: 'Staff not found' });
+    res.json(staff);
+  } catch (error) {
+    next(error);
+  }
+}; 

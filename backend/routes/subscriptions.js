44fe9902b3a560fa1b886e6c3b3b7a512106ad58
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-07-13 18:43:09 +0300

    Update subscriptions.js

--- backend/routes/subscriptions.js
+++ backend/routes/subscriptions.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const subscriptionController = require('../controllers/subscriptionController');
-const { protect, authorize } = require('../middleware/authMiddleware');
+const { authenticateToken, authorize } = require('../middleware/authMiddleware');
 
 // Protect all routes
-router.use(protect);
+router.use(authenticateToken);
 
 // --- PLAN ROUTES ---
 router.get('/plans', subscriptionController.getPlans);

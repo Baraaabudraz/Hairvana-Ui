2b7df9644b23f22ba58a33efc00569f5e7fcd0f5
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-07-13 12:13:07 +0300

    Update index.tsx

--- src/pages/dashboard/subscriptions/index.tsx
+++ src/pages/dashboard/subscriptions/index.tsx
@@ -711,7 +711,7 @@ export default function SubscriptionsPage() {
                   <div className="flex items-center space-x-4">
                     <div className="relative">
                       <Avatar className="h-12 w-12">
-                        <AvatarImage src={`https://images.pexels.com/photos/3993449/pexels-photo-3993449.jpeg?auto=compress&cs=tinysrgb&w=100&h=100&dpr=2`} alt={subscription.salonName} />
+                        <AvatarImage src={'https://images.pexels.com/photos/3993449/pexels-photo-3993449.jpeg?auto=compress&cs=tinysrgb&w=100&h=100&dpr=2'} alt={subscription.salonName} />
                         <AvatarFallback>{subscription.salonName.split(' ').map(n => n[0]).join('')}</AvatarFallback>
                       </Avatar>
                       <div className="absolute -bottom-1 -right-1 bg-white rounded-full p-1">

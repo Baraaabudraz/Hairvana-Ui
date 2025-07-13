456b257db41899dfc8e871dcd2c252e388d6ffd9
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-07-13 12:42:16 +0300

    Update build-server.js

--- build-server.js
+++ build-server.js
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 
-const { spawn } = require('child_process');
-const path = require('path');
+import { spawn } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Memory-optimized build script for server environments
 async function buildServer() {

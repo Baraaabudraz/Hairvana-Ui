ac4d4fdb4996a28cc08532c4450bb6e0ef5327db
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-07-14 12:54:04 +0300

    Create stripeWebhook.js

---
+++ backend/routes/Api/stripeWebhook.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const mobilePaymentController = require('../../controllers/Api/mobilePaymentController');
+
+// Stripe webhook route (must use raw body)
+router.post('/webhook', express.raw({ type: 'application/json' }), mobilePaymentController.stripeWebhook);
+
+module.exports = router; 

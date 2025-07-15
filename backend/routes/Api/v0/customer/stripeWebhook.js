66aba466c4dd7e3458ec0d334e0d9677c59df57e
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-07-15 14:23:32 +0300

    Update stripeWebhook.js

--- backend/routes/Api/v0/customer/stripeWebhook.js
+++ backend/routes/Api/v0/customer/stripeWebhook.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const mobilePaymentController = require('../../controllers/Api/mobilePaymentController');
+const mobilePaymentController = require('../../../../controllers/Api/customer/mobilePaymentController');
 
 // Stripe webhook route (must use raw body)
 router.post('/webhook', express.raw({ type: 'application/json' }), mobilePaymentController.stripeWebhook);

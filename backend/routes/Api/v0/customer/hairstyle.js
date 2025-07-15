420951c78f7db2bca86c12d708f8388efb58b595
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-07-15 14:23:20 +0300

    Update hairstyle.js

--- backend/routes/Api/v0/customer/hairstyle.js
+++ backend/routes/Api/v0/customer/hairstyle.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const hairstyleController = require('../../controllers/Api/hairstyleController');
+const hairstyleController = require('../../../../controllers/Api/customer/hairstyleController');
 const { getHairstylesValidation, getHairstyleByIdValidation } = require('../../../../validation/hairstyleValidation');
 const validate = require('../../../../middleware/validate');
 

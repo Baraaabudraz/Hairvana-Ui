8e9c792e669db7f4ab75ae4a1e62586ac95a9247
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-07-16 15:08:17 +0300

    Update mobileAuth.js

--- backend/routes/Api/v0/customer/mobileAuth.js
+++ backend/routes/Api/v0/customer/mobileAuth.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const mobileAuthController = require('../../../../controllers/Api/customer/auth/mobileAuthController');
 const { registerValidation, loginValidation } = require('../../../../validation/mobileAuthValidation');
 const validate = require('../../../../middleware/validate');
+const { authenticateToken } = require('../../../../middleware/authMiddleware');
 
 router.post('/register', registerValidation, validate, mobileAuthController.register);
 router.post('/login', loginValidation, validate, mobileAuthController.login);
+router.post('/logout', authenticateToken, mobileAuthController.logout);
 
 module.exports = router; 

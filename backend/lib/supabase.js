b7deaa881aaf00d9e6fe146f5ddc81b5fcf847a3
yusufmsabeh <yussef.msabeh@gmail.com>
2025-07-23 11:39:20 +0300

    fix: edit code

--- backend/lib/supabase.js
+++ backend/lib/supabase.js
@@ -1,14 +1,13 @@
-const { Sequelize } = require('sequelize');
-const path = require('path');
-require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
-console.log(process.env.DB_NAME);
+const { Sequelize } = require("sequelize");
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT || 'mysql',
+    dialect: process.env.DB_DIALECT || "mysql",
     logging: false,
   }
 );

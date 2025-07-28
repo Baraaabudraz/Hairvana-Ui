0a1e0f0870135f08bfe787d727777df141943817
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-07-28 13:24:32 +0300

    Update dashboard-layout.tsx

--- src/layouts/dashboard-layout.tsx
+++ src/layouts/dashboard-layout.tsx
@@ -1,19 +1,22 @@
 import { Outlet } from 'react-router-dom';
 import { Sidebar } from '../components/dashboard/sidebar';
 import { Header } from '../components/dashboard/header';
+import { ProtectedRoute } from '../components/protected-route';
 
 export default function DashboardLayout() {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <Sidebar />
-      <div className="lg:pl-72">
-        <Header />
-        <main className="py-6">
-          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-            <Outlet />
-          </div>
-        </main>
+    <ProtectedRoute requiredResource="dashboard" requiredAction="view">
+      <div className="min-h-screen bg-gray-50">
+        <Sidebar />
+        <div className="lg:pl-72">
+          <Header />
+          <main className="py-6">
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+              <Outlet />
+            </div>
+          </main>
+        </div>
       </div>
-    </div>
+    </ProtectedRoute>
   );
 }

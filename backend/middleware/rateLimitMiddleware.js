f6f10245de1bc6515fc2481f5faca345dcdf932a
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-08-06 12:03:58 +0300

    Update rateLimitMiddleware.js

--- backend/middleware/rateLimitMiddleware.js
+++ backend/middleware/rateLimitMiddleware.js
@@ -2,9 +2,9 @@ const rateLimit = require('express-rate-limit');
 
 // Rate limiting for login attempts
 const loginRateLimit = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 5 * 60 * 1000, // 05 minutes
   max: 5, // limit each IP to 5 requests per windowMs
-  message: 'Too many login attempts, please try again after 15 minutes',
+  message: 'Too many login attempts, please try again after 5 minutes',
   standardHeaders: true,
   legacyHeaders: false,
 });
@@ -20,9 +20,9 @@ const registerRateLimit = rateLimit({
 
 // Rate limiting for password change attempts
 const passwordChangeRateLimit = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 3, // limit each IP to 3 password change attempts per 15 minutes
-  message: 'Too many password change attempts, please try again after 15 minutes',
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 3, // limit each IP to 3 password change attempts per 5 minutes
+  message: 'Too many password change attempts, please try again after 5 minutes',
   standardHeaders: true,
   legacyHeaders: false,
 });

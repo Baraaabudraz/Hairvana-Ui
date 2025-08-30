6fd04288f33eb1f15099ec06704c278d56d5b1b2
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-08-30 19:04:01 +0300

    Update index.tsx

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -21,7 +21,7 @@ export default function DashboardPage() {
     }
   }, []);
 
-  if (user?.role === "user") {
+  if (user?.role === "customer") {
     return (
       <div className="flex items-center justify-center h-96">
         <div className="text-center">

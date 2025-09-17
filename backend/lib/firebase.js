76fee3b52187868bb1351691737be9c8762296ed
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-09-17 16:47:52 +0300

    Update firebase.js

--- backend/lib/firebase.js
+++ backend/lib/firebase.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../config/up-lancer-fbcd320b3801.json');
+const serviceAccount = require('../config/up-lancer-ac146cc55400.json');
 
 if (!admin.apps.length) {
   admin.initializeApp({
